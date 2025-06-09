c3bb88942ad1cdf0892c16c96e076603f8b0ea16
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-06-10 01:28:52 +0600

    landing page

--- personal-portfolio/src/app/page.js
+++ personal-portfolio/src/app/page.js
@@ -1,7 +1,10 @@
-import Image from "next/image";
+'use client'
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Hero/>
+    </div>
   );
 }
---
+++ personal-portfolio/src/components/Hero.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Image from "next/image";
+
+const Hero = () => {
+  return <div className="h-screen grid place-items-center">
+    <div>
+      <div className="flex flex-col items-center justify-center gap-y-3 font-light capitalize">
+        <div className="flex items-center justify-center">
+          <Image
+            src={"/person.jpeg"}
+            alt="Person Image"
+            width={400}
+            height={400}
+            priority={true}
+            className="h-auto w-[150px]"
+          />
+          <span className="absolute text-3xl font-semibold text-white">Hi</span>
+          </div>
+          <h1 className="text-center text-3xl font-bold tracking-wider text-gray-500">
+            My Name Is Mehedi Hasan Alif &
+          </h1>
+          <p className="text-lg tracking-wider text-gray-700">
+            I like programming 🤗{" "}
+          </p>
+        </div>
+        <div>
+          <a href="#">Icon</a>
+        </div>
+        <a
+          href="#"
+          className="mx-auto mt-7 block w-max rounded-lg bg-red-400 px-3 py-1 font-light capitalize tracking-wider text-white hover:bg-red-500 transition-colors"
+        >
+          Talk to me
+        </a>
+      </div>
+    </div>
+};
+
+export default Hero;
