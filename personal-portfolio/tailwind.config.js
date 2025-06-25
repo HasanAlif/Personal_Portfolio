e2a90275f58e5f538c7fd5f296befd1004466484
HasanAlif <alifalmehedihasan@gmail.com>
2025-06-25 14:41:47 +0600

    Adding Animation with Framer Motion

---
+++ personal-portfolio/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+    screens: {
+      xl: {max: '1440px'},
+      lg: {max: '1024px'},
+      md: {max: '768px'},
+      sm: {max: '640px'},
+    }
+  },
+  plugins: [],
+};
