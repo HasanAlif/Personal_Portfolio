1c35bb72cbb204e6e5d53bb7f591b7c6bd58a36b
HasanAlif <alifalmehedihasan@gmail.com>
2025-06-26 00:18:43 +0600

    Experience & Education section

--- personal-portfolio/src/app/page.js
+++ personal-portfolio/src/app/page.js
@@ -1,12 +1,14 @@
 'use client'
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
       <About/>
+      <Experience/>
     </div>
   );
 }
